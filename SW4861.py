f577f54401876090c46af3f3223e3a5046e264a3
Seawo <89066816+Seawo@users.noreply.github.com>
2023-12-23 17:57:42 +0900

    sw 회문

--- SW4861.py
+++ SW4861.py
@@ -3,5 +3,10 @@
 for T in range(int(input())):
     
     N,M = map(int,input().split())
-   # for Nnum in range(N):
+    for Nnum in range(N):
+        word = list(input())
+        check = word[::-1]
+        if word == check:
+            word= ''.join(word)
+            print(word)
     
