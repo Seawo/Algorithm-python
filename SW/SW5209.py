125f299400fed645330c049822437a89de0169cf
Seawo <89066816+Seawo@users.noreply.github.com>
2023-12-29 22:30:50 +0900

    SW5209 미해결

---
+++ SW/SW5209.py
@@ -0,0 +1,18 @@
+
+
+for T in range(int(input())):
+    
+    N = int(input())
+    V = [list(map(int, input().split())) for _ in range(N)]
+    
+    check = [0]*N
+    res = 12312
+    
+    # 재귀??    
+# 3
+# 3
+# 73 21 21
+# 11 59 40
+# 24 31 83
+
+#1 63
